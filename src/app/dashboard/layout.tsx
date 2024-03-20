b2437d48d8ab489b8ea03c972d7f099cc333d2e7
sukhsingh-dev <sukhjindersinghmca@outlook.com>
2024-03-20 16:29:29 +0530

    added page route protection for all pages

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,19 @@
 import NavBar from "@/shared/components/Navbar"
 import Sidebar from "@/shared/components/Sidebar"
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode
 }) {
+  const session = await getServerSession()
+
+  if (!session || !session?.user) {
+    redirect('/')
+  }
+
   return (
     <section className="page-bg dashboard-layout">
       <NavBar />
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,4 @@
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-
 export default async function Dashboard() {
-  const session = await getServerSession()
-
-  if (!session || !session?.user) {
-    redirect('/api/auth/signin')
-  }
-
   return (
     <main>
       <h1>this should be protected</h1>
