32de505de8abeaa6f0b15c2b818fb1f7b0fcf2ab
sukhsingh-dev <sukhjindersinghmca@outlook.com>
2024-03-22 11:15:03 +0530

    moved category data inside client component due to vercel error

---
+++ src/app/dashboard/categories/client/table.tsx
@@ -0,0 +1,50 @@
+/* eslint-disable @next/next/no-img-element */
+
+'use client'
+import Link from "next/link"
+import DeleteItem from "@/shared/components/DeleteItem";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const TableData = () => {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    const getCategories = async () => {
+      const res = await axios.get('/api/categories');
+      return res;
+    }
+
+    getCategories().then((result) => {
+      const inside = result.data;
+      setData(inside)
+    })
+  }, [])
+
+  return (
+    data.map((category: any, index: number) => {
+      return (
+        <tr key={category._id}>
+          <td>{index + 1}</td>
+          <td><img src={category.categoryImg} alt="image" width={40} height={40} /></td>
+          <td>{category.categoryName}</td>
+          <td><span className="bg-span" style={{ background: category.categoryBg }} /></td>
+          <td>
+            {
+              category.categoryAttributes.map((attr: any) => {
+                return (
+                  <span className="span-attribute" key={attr.value}>{attr.label}</span>
+                )
+              })
+            }
+          </td>
+          <td>
+            <Link href={`/dashboard/categories/edit/${category._id}`} className="btn-link btn-edit" >Edit</Link>
+            <DeleteItem id={category._id} />
+          </td>
+        </tr>
+      )
+    })
+  )
+}
+
+export default TableData;
--- src/app/dashboard/categories/page.tsx
+++ src/app/dashboard/categories/page.tsx
@@ -1,11 +1,6 @@
-/* eslint-disable @next/next/no-img-element */
 import Link from "next/link"
-import DeleteItem from "@/shared/components/DeleteItem";
-
+import TableData from "./client/table"
 const CategoriesPage = async () => {
-  const res = await fetch(`${process.env.APP_URL}/api/categories`, { next: { revalidate: 10 } });
-  const data = await res.json()
-
   return (
     <main className="sn-main-table" >
       <div className="sn-main-table-header">
@@ -24,31 +19,7 @@ const CategoriesPage = async () => {
           </tr>
         </thead>
         <tbody>
-          {
-            data.map((category: any, index: number) => {
-              return (
-                <tr key={category._id}>
-                  <td>{index + 1}</td>
-                  <td><img src={category.categoryImg} alt="image" width={40} height={40} /></td>
-                  <td>{category.categoryName}</td>
-                  <td><span className="bg-span" style={{ background: category.categoryBg }} /></td>
-                  <td>
-                    {
-                      category.categoryAttributes.map((attr: any) => {
-                        return (
-                          <span className="span-attribute" key={attr.value}>{attr.label}</span>
-                        )
-                      })
-                    }
-                  </td>
-                  <td>
-                    <Link href={`/dashboard/categories/edit/${category._id}`} className="btn-link btn-edit" >Edit</Link>
-                    <DeleteItem id={category._id} />
-                  </td>
-                </tr>
-              )
-            })
-          }
+          <TableData />
         </tbody>
       </table>
     </main>
