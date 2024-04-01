aa77fb6aafc686e7f4cdcc2b46d4c06c2ab49fcf
sukhsingh-dev <sukhjindersinghmca@outlook.com>
2024-04-01 16:08:13 +0530

    added multiple product search for categories

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -51,7 +51,8 @@ export const GET = async (request: NextRequest) => {
   const id= product.get("id")
   const filters= product.get("filters")
   const category= product.get("category")
-  
+  const categoriesArray = category?.split(',')
+
   let data;
 
   try {
@@ -63,7 +64,7 @@ export const GET = async (request: NextRequest) => {
         data = await Product.findOne({ _id: id });
       }
     } else {
-      data = await Product.find({ 'productCategory.label' : category }).sort({ updatedAt: -1 });
+      data = await Product.find({ 'productCategory.label' : { $in: categoriesArray } }).sort({ updatedAt: -1 });
     }
 		return NextResponse.json(data, { status: 200 });
 	} catch (error) {
